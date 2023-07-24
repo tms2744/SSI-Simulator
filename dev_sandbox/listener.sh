af04cf2e2ac4df126c7f84d204c1014a574a7afe
tms2744 <tms2744@g.rit.edu>
2023-07-24 18:46:44 -0400

    Some more tooling in dev_sandbox, trying to get netcat to work

--- dev_sandbox/listener.sh
+++ dev_sandbox/listener.sh
@@ -4,10 +4,15 @@ num=$1
 round=$2
 shared_dir=$3
 timeout=$4
+loop=0
 
 echo "I'm listening" > "/purple/results/data.txt"
 #echo "Again" > "${shared_dir}/results/${round}/dsta${num}.txt"
-nc -lk 80  > "${shared_dir}/results/${round}/net_data${num}.txt"
 
+echo "thank you" | nc -lk 80  >> "${shared_dir}/results/${round}/net_data${num}.txt"
 
+#while [ $loop -ne -1 ]
+#do
+#	nc -lk 80 >> "${shared_dir}/results/${round}/net_data${num}.txt"
+#done
 
