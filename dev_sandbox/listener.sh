63b7a62507c8371724dc340c317b2a2808fe6424
tms2744 <tms2744@g.rit.edu>
2023-07-25 18:00:00 -0400

    Worked in dev_sandbox some more, prommisnig netcat results

--- dev_sandbox/listener.sh
+++ dev_sandbox/listener.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 num=$1
-round=$2
-shared_dir=$3
-timeout=$4
+total=$2
+round=$3
+shared_dir=$4
+timeout=$5
 loop=0
 
 echo "I'm listening" > "/purple/results/data.txt"
 #echo "Again" > "${shared_dir}/results/${round}/dsta${num}.txt"
 
-echo "thank you" | nc -lk 80  >> "${shared_dir}/results/${round}/net_data${num}.txt"
+if [ $num == $total ]; then
+	#nc -lk 80 >> "${shared_dir}/results/${round}/net_data${num}.txt"
+	nc -lk 80 >> /bin/bash
+	#echo "Hi There"
+else
+	mkfifo backpipe
+	echo "172.50.0.$(($num+2))"
+	nc -lk 80 <backpipe | nc "172.50.0.$(($num+2))" 80 1>backpipe
+fi
 
 #while [ $loop -ne -1 ]
 #do
