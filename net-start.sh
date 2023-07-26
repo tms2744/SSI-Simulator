676110e362877b7dc1d5115fa23007affabc97aa
tms2744 <tms2744@g.rit.edu>
2023-07-26 15:02:47 -0400

    Changed the flags a bit on the inital connection, the tunnel now seems to conitnue through the loop.

--- net-start.sh
+++ net-start.sh
@@ -6,7 +6,7 @@ i=0
 while [ $i -ne -1 ] 
 do
 	echo "iteration ${i}" >> "/purple/results/rand.txt"
-	printf "hostname >> /purple/results/res.txt\n" | nc $target 80
+	printf "hostname >> /purple/results/res.txt\n" | nc -N $target 80
 	i=$(($i+1))
 	#sleep 1 
 done
