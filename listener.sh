d59ec4b15d8fd1e486bca958d3eba5746c47d8f7
tms2744 <tms2744@g.rit.edu>
2023-08-10 19:02:25 -0400

    Worked on nt.sh mostly, and attempted to get nested interlopability working. However, there is still alot of work to be done. Nesting works, execution even works, but there is no persistance. THere could also be potential issues with the sheel being generated by ncat, but theat will be looked at later.

--- listener.sh
+++ listener.sh
@@ -12,7 +12,9 @@ echo "I'm listening" > "/purple/results/data.txt"
 #echo "Again" > "${shared_dir}/results/${round}/dsta${num}.txt"
 
 
-ncat --exec "/bin/bash" -o "/purple/results/${round}/listen_$HOSTNAME.txt" -l 80 --keep-open
+#ncat -o "/purple/results/${round}/listen_$HOSTNAME.txt" -l 80
+
+ncat --exec "/bin/sh" -o "/purple/results/${round}/listen_$HOSTNAME.txt" -l 80 --keep-open
 
 #if [ $num == $total ]; then
 #	#nc -lk 80 >> "${shared_dir}/results/${round}/net_data${num}.txt"
