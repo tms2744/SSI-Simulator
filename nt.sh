fdebeda099e13c59dba70f81b83aa48a24a14ce6
tms2744 <tms2744@g.rit.edu>
2023-08-11 16:04:42 -0400

    Progress was being made with ncat tunneling, but after swititching to a cerntralized command launcher (the new launch.sh), problems with multiple commands began manifesting again. The command handling was greatly simplifed with the addition of launch.sh, and command genration was simplifed as well.

--- nt.sh
+++ nt.sh
@@ -2,35 +2,15 @@
 
 target=$1
 round=$2
-id=$3
-cmd=${@:4}
+#id=$3
+cmd=${@:3}
 i=0
 #sleep-time=$(( $RANDOM % 10 + 1 ))
 
-
-#The basic premise is that a perisistnat ncat conneciton is made and commands are randmomly selected and then feed into the first
-#node(If ncat is the first tunnel). THere are alot proprms with this however, while nestting works the persistanc edoes not, and
-#it is unclear if this due to the "client" or the "server". AS mentioned in alt_internal.py command feeding will bprobably be
-#handled by a third script in the near future.
-
 echo "ID is: $id, Command is: printf \"${cmd}\\n\" | nc -N ${target} 80" >> "/purple/results/${round}/commands.txt"
 
-if [ "${id}" == "0" ]; then
-	tmux new -d -s mySession
-	tmux send-keys -t mySession.0 "printf \"${cmd}\\n\" | ncat -o \"/purple/results/${round}/$HOSTNAME.txt\" ${target} 80" Enter
-	tmux send-keys -t mySession.0 "hostname" Enter
-	while [ $i -ne -1 ]
-	do
-		sleep_time=$(( $RANDOM % 10 + 1 ))
-		cmd2=$(shuf -n 1 /purple/cmd.txt)
-		echo $cmd2 >> "/purple/results/${round}/cmds"
-		tmux send-keys -t mySession.0 "$cmd2" Enter
-		i=$(($i+1))
-		sleep $sleep_time
-	done
-else
-	printf "${cmd}\n" | ncat -o "/purple/results/${round}/$HOSTNAME.txt" $target 80
-fi
+printf "${cmd}\n" | ncat -o "/purple/results/${round}/$HOSTNAME.txt" $target 80
+
 #printf "${cmd}\n" | nc -N $target 80 >> "/purple/results/${round}/results.txt"
 
 
