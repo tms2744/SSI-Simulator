434ab9924626acce15093fa5bfcde3d0b996889a
tms2744 <tms2744@g.rit.edu>
2023-08-15 16:11:31 -0400

    Continued to try and figure out ncat tunnels without much luck. Possible success in removeing the compersome first hop preset. Ran some experiments with an all SSH tunnel with promising results as well. some incidental changes to launch.sh due to expermentation and reconfigurations.

--- launch.sh
+++ launch.sh
@@ -8,17 +8,18 @@ echo "${cmd}" >> "/purple/results/${round}/cmds.txt"
 
 tmux new -d -s mySession
 
-#tmux send-keys -t mySession.0 "/bin/bash -i" Enter
+tmux send-keys -t mySession.0 "/bin/bash -i" Enter
 
-#tmux send-keys -t mySession.0 "${cmd}" Enter
+tmux send-keys -t mySession.0 "${cmd}" Enter
 
 while [ $i -ne -1 ]
 do
 	sleep_time=$(( $RANDOM % 10 + 1 ))
 	cmd2=$(shuf -n 1 /purple/cmd.txt)
-	#tmux send-keys -t mySession.0 "${cmd2}" Enter
+	tmux send-keys -t mySession.0 "${cmd2}" Enter
 	echo "${cmd2}"
 	i=$(($i+1))
 	sleep $sleep_time
-done | sudo bash -i ${cmd}
+done
+#| sudo bash -i ${cmd}
 
